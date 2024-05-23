4ea3d1aa5ae41852b6ec4e25daca75da4f6b33c8
Douglas <douglaslima2363@gmail.com>
2024-05-22 23:01:55 -0300

    Create in start jogo

---
+++ jogo_react/src/components/StartScree.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StartScree.css";
+
+const StartScree = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button className="btn">Começar o jogo</button>
+    </div>
+  );
+};
+
+export default StartScree;
---
+++ jogo_react/src/data/word.js
@@ -0,0 +1,8 @@
+export const worlist = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Maçã", "Banana", "Uva", "Pera", "Melancia"],
+  corpo: ["Cabeça", "Braço", "Perna", "Mão", "Pé"],
+  computador: ["Teclado", "Mouse", "Monitor", "CPU", "Placa de vídeo"],
+  programação: ["Javascript", "React", "HTML", "CSS", "Python"],
+  alimento: ["Arroz", "Feijão", "Carne", "Peixe", "Frango"],
+};
