b4fd5da3aadedf52446b7b636b2fc48ba19673f7
Douglas <douglaslima2363@gmail.com>
2024-05-22 23:46:02 -0300

    Criando os componentes do jogo

--- jogo_react/src/App.js
+++ jogo_react/src/App.js
@@ -1,11 +1,40 @@
-import logo from "./logo.svg";
+//css
 import "./App.css";
+//React
+import { useCallback, useEffect, useState } from "react";
+//data
+import { worlist } from "./data/word";
+//compoenents
 import StartScree from "./components/StartScree";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  {
+    id: 1,
+    name: "Start",
+  },
+  {
+    id: 2,
+    name: "Game",
+  },
+  {
+    id: 3,
+    name: "End",
+  },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(worlist);
+
+  console.log(words);
+
   return (
     <div className="App">
-      <StartScree />
+      {gameStage === "Start" && <StartScree />}
+      {gameStage === "Game" && <Game />}
+      {gameStage === "End" && <GameOver />}
     </div>
   );
 }
